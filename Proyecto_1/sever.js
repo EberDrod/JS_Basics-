e82f56af87b30d9c0bc27b836a2e422cf7c2bb19
Eber Peña <eberg.pena@gmail.com>
2020-03-20 15:06:14 -0600

    Se configuro el archivo para que leyera server.js en el navegador

---
+++ Proyecto_1/sever.js
@@ -0,0 +1,21 @@
+nombre = 'Eber';
+Apellido = "Peña";
+
+mensaje = 'Y entonces dije \`Buen Curso`';
+
+mensaje = 'JAVA' + 'SCRIPT';
+
+mensaje = `Aprendiendo JavaScript, CSS, HTML para ser Frontend`;
+
+let tecnologia = "JavaScript" + ' ';
+
+console.log(tecnologia.repeat(10));
+
+//Nos regleza la frase en un arreglo
+console.log(mensaje.split(','));
+
+//Reemplaza los mensajes la letra establecida por la nueva asginación
+console.log(mensaje.replace('CSS', 'PHP'));
+
+//Nos regresa un booleano CSS * REACT
+console.log(mensaje.includes('PHP'));
